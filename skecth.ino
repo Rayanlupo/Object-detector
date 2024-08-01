cc26649df702a48013acade26ba7dfd42e106762
Rayan <154925122+Rayanlupo@users.noreply.github.com>
2024-08-01 17:31:04 +0200

    Update skecth.ino

--- skecth.ino
+++ skecth.ino
@@ -6,21 +6,21 @@ int TrigPin = 33;
 int EchoPin = 32;
 int ButtonPin = 26;
 int BuzzerPin = 14;
-int angle = 0;
+
 void setup() {
   servo1.attach(ServoPin);
   pinMode(TrigPin, OUTPUT);
   pinMode(EchoPin, INPUT);
   pinMode(BuzzerPin, OUTPUT);
-  pinMode(ButtonPin, INPUT);
+  pinMode(ButtonPin, INPUT_PULLUP);
   Serial.begin(115200);
 }
 
 void loop() {
   // Start servo operation when the button is pressed
   if (digitalRead(ButtonPin) == HIGH) {
-    while (true) {  
-      angle = angle + 5;
+    while (true) {  // Keep running the loop
+      // Measure distance using the ultrasonic sensorù
       noTone(BuzzerPin);
       digitalWrite(TrigPin, LOW);
       delayMicroseconds(2);
@@ -33,12 +33,14 @@ void loop() {
 
       // Check if an object is detected within 40 cm
       if (distance <= 40) {
+        Serial.print("Object Detected");
         tone(BuzzerPin, 262, 250); // Sound the buzzer
-        servo1.write(angle); // Stop servo (neutral position for continuous servo)
+        servo1.write(180);
+        delay(1000) ;
       } else {
-        servo1.write(angle); // Continue rotating servo (speed depends on servo type)
+        servo1.write(0); // Continue rotating servo (speed depends on servo type)
       }
-      
+
       delay(15); // Small delay for stability
     }
   }
