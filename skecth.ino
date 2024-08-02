c21017945e2b2175b8c656057af30a36b61d449d
Rayan <154925122+Rayanlupo@users.noreply.github.com>
2024-08-02 12:30:57 +0200

    Update skecth.ino

--- skecth.ino
+++ skecth.ino
@@ -1,21 +1,25 @@
 #include <ESP32Servo.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+
 Servo servo1;
 int ServoPin = 18;
 int TrigPin = 33;
 int EchoPin = 32;
 int ButtonPin = 26;
 int BuzzerPin = 14;
-Adafruit_SSD1306 display(128,64, &Wire, -1);
+int angle = 0;
+int rotations = 0;
+bool objectDetected = false;
+Adafruit_SSD1306 display(128, 64, &Wire, -1);
 
 void setup() {
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
   display.clearDisplay();
   display.setTextSize(1);
   display.setTextColor(WHITE);
-  display.setCursor(18,15);
-  
+  display.setCursor(18, 15);
+
   servo1.attach(ServoPin);
   pinMode(TrigPin, OUTPUT);
   pinMode(EchoPin, INPUT);
@@ -25,11 +29,11 @@ void setup() {
 }
 
 void loop() {
-  // Start servo operation when the button is pressed
   if (digitalRead(ButtonPin) == HIGH) {
+    rotations = 0;
+    while (rotations < 2) {  
 
-    while (true) {  // Keep running the loop
-      // Measure distance using the ultrasonic sensorù
+    
       noTone(BuzzerPin);
       digitalWrite(TrigPin, LOW);
       delayMicroseconds(2);
@@ -40,17 +44,35 @@ void loop() {
       long duration = pulseIn(EchoPin, HIGH);
       int distance = (duration * 0.034 / 2);
 
-      
-      if (distance <= 40) {
+      while (distance <= 40) {
         display.println("OBJECT DETECTED");
-         display.display();
+        display.display();
         Serial.print("Object Detected\n");
         tone(BuzzerPin, 262, 250);
-        servo1.write(180);
-        delay(1000) ;
-      } else {
-        servo1.write(0); 
+        servo1.write(angle);  
+        objectDetected = true;
+      }  
+        if (objectDetected) {
+          objectDetected = false;
+          display.clearDisplay();
+          display.setCursor(18, 15);
+          display.display();
         }
+        servo1.write(angle);
+        angle += 20;
+        if (angle >= 180) {
+          angle = 0;
+          rotations++;
+        }
+      
+
+      delay(1000);
+    }
+    servo1.write(90);
+    servo1.detach();
+  }
+}
+
 
       delay(15); 
       
