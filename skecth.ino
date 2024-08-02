ba919763c6e5fd785fca060b5b3dc83704946cde
Rayan <154925122+Rayanlupo@users.noreply.github.com>
2024-08-02 17:32:56 +0200

    Update skecth.ino

--- skecth.ino
+++ skecth.ino
@@ -8,8 +8,11 @@ int TrigPin = 33;
 int EchoPin = 32;
 int ButtonPin = 26;
 int BuzzerPin = 14;
+int ResetPin = 35;
 int angle = 0;
 int rotations = 0;
+int distance;
+int measureDistance();
 bool objectDetected = false;
 Adafruit_SSD1306 display(128, 64, &Wire, -1);
 
@@ -25,57 +28,60 @@ void setup() {
   pinMode(EchoPin, INPUT);
   pinMode(BuzzerPin, OUTPUT);
   pinMode(ButtonPin, INPUT_PULLUP);
+  pinMode(ResetPin, INPUT);
+  digitalWrite(ButtonPin, LOW);
   Serial.begin(115200);
 }
 
 void loop() {
+
+int distance = measureDistance();
+  
   if (digitalRead(ButtonPin) == HIGH) {
     rotations = 0;
-    while (rotations < 2) {  
-
-    
+    while (rotations <= 2) {  
+      
       noTone(BuzzerPin);
-      digitalWrite(TrigPin, LOW);
-      delayMicroseconds(2);
-      digitalWrite(TrigPin, HIGH);
-      delayMicroseconds(10);
-      digitalWrite(TrigPin, LOW);
-
-      long duration = pulseIn(EchoPin, HIGH);
-      int distance = (duration * 0.034 / 2);
-
-      while (distance <= 40) {
+distance = measureDistance();
+      
+      servo1.write(angle);
+      angle += 20;
+        if (angle >= 180) {
+          servo1.write(angle);
+          angle = 0;
+          rotations++;
+          if (rotations == 2);{
+            break;
+          }
+        }
+      
+      if (distance <= 40) {
         display.println("OBJECT DETECTED");
         display.display();
         Serial.print("Object Detected\n");
         tone(BuzzerPin, 262, 250);
         servo1.write(angle);  
         objectDetected = true;
+        delay(1000);
       }  
-        if (objectDetected) {
-          objectDetected = false;
-          display.clearDisplay();
-          display.setCursor(18, 15);
-          display.display();
-        }
-        servo1.write(angle);
-        angle += 20;
-        if (angle >= 180) {
-          angle = 0;
-          rotations++;
-        }
+      else{
+        continue;
+      }
       
+      delay(1000);  
 
-      delay(1000);
-    }
-    servo1.write(90);
-    servo1.detach();
-  }
-}
-
-
-      delay(15); 
-      
     }
   }
 }
+  int measureDistance(){
+digitalWrite(trigPin, LOW);
+delayMicroseconds(2);
+digitalWrite(trigPin, HIGH);
+delayMicroseconds(10);
+digitalWrite(trigPin, LOW);
+
+  long duration = pulseIn(echoPin, HIGH);
+  distance = (duration * 0.034 / 2);
+  return 0;
+}
+
